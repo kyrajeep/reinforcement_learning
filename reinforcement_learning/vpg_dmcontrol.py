5bd11e604cd02190ad3b0bffff077cff9e5f4414
NoNameAA <bbmmddkk@gmail.com>
2022-10-24 20:02:11 -0700

    All the imports are running but matplotlib

---
+++ reinforcement_learning/vpg_dmcontrol.py
@@ -0,0 +1,168 @@
+'''
+#@title Run to install MuJoCo and `dm_control`
+import distutils.util
+import subprocess
+if subprocess.run('nvidia-smi').returncode:
+  raise RuntimeError(
+      'Cannot communicate with GPU. '
+      'Make sure you are using a GPU Colab runtime. '
+      'Go to the Runtime menu and select Choose runtime type.')
+
+print('Installing dm_control...')
+!pip install -q dm_control>=1.0.8
+
+# Configure dm_control to use the EGL rendering backend (requires GPU)
+%env MUJOCO_GL=egl
+'''
+print('Checking that the dm_control installation succeeded...')
+try:
+  from dm_control import suite
+  env = suite.load('cartpole', 'swingup')
+  pixels = env.physics.render()
+except Exception as e:
+  raise e from RuntimeError(
+      'Something went wrong during installation. Check the shell output above '
+      'for more information.\n'
+      'If using a hosted Colab runtime, make sure you enable GPU acceleration '
+      'by going to the Runtime menu and selecting "Choose runtime type".')
+else:
+  del pixels, suite
+
+#!echo Installed dm_control $(pip show dm_control | grep -Po "(?<=Version: ).+")
+#@title All `dm_control` imports required for this tutorial
+
+# The basic mujoco wrapper.
+from dm_control import mujoco
+
+# Access to enums and MuJoCo library functions.
+from dm_control.mujoco.wrapper.mjbindings import enums
+from dm_control.mujoco.wrapper.mjbindings import mjlib
+
+# PyMJCF
+from dm_control import mjcf
+
+# Composer high level imports
+from dm_control import composer
+from dm_control.composer.observation import observable
+from dm_control.composer import variation
+
+# Imports for Composer tutorial example
+from dm_control.composer.variation import distributions
+from dm_control.composer.variation import noises
+from dm_control.locomotion.arenas import floors
+
+# Control Suite
+from dm_control import suite
+
+# Run through corridor example
+from dm_control.locomotion.walkers import cmu_humanoid
+from dm_control.locomotion.arenas import corridors as corridor_arenas
+from dm_control.locomotion.tasks import corridors as corridor_tasks
+
+# Soccer
+from dm_control.locomotion import soccer
+
+# Manipulation
+from dm_control import manipulation
+#@title Other imports and helper functions
+
+# General
+import copy
+import os
+import itertools
+from IPython.display import clear_output
+import numpy as np
+
+# Graphics-related
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+import PIL.Image
+# Internal loading of video libraries.
+
+# Use svg backend for figure rendering
+#%config InlineBackend.figure_format = 'svg'
+'''
+import matplotlib_inline.backend_inline
+
+if isinstance(ipython_format, str):
+    ipython_format = [ipython_format]
+    matplotlib_inline.backend_inline.set_matplotlib_formats(*ipython_format)
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('retina')
+'''
+# Font sizes
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+# Inline video helper function
+if os.environ.get('COLAB_NOTEBOOK_TEST', False):
+  # We skip video generation during tests, as it is quite expensive.
+  display_video = lambda *args, **kwargs: None
+else:
+  def display_video(frames, framerate=30):
+    height, width, _ = frames[0].shape
+    dpi = 70
+    orig_backend = matplotlib.get_backend()
+    matplotlib.use('Agg')  # Switch to headless 'Agg' to inhibit figure rendering.
+    fig, ax = plt.subplots(1, 1, figsize=(width / dpi, height / dpi), dpi=dpi)
+    matplotlib.use(orig_backend)  # Switch back to the original backend.
+    ax.set_axis_off()
+    ax.set_aspect('equal')
+    ax.set_position([0, 0, 1, 1])
+    im = ax.imshow(frames[0])
+    def update(frame):
+      im.set_data(frame)
+      return [im]
+    interval = 1000/framerate
+    anim = animation.FuncAnimation(fig=fig, func=update, frames=frames,
+                                   interval=interval, blit=True, repeat=False)
+    return HTML(anim.to_html5_video())
+
+# Seed numpy's global RNG so that cell outputs are deterministic. We also try to
+# use RandomState instances that are local to a single cell wherever possible.
+np.random.seed(42)
+
+
+#@title A static model {vertical-output: true}
+
+static_model = """
+<mujoco>
+  <worldbody>
+    <light name="top" pos="0 0 1"/>
+    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+  </worldbody>
+</mujoco>
+"""
+physics = mujoco.Physics.from_xml_string(static_model)
+pixels = physics.render()
+PIL.Image.fromarray(pixels)
+
+#@title Making a video {vertical-output: true}
+
+duration = 2    # (seconds)
+framerate = 30  # (Hz)
+
+# Visualize the joint axis
+scene_option = mujoco.wrapper.core.MjvOption()
+scene_option.flags[enums.mjtVisFlag.mjVIS_JOINT] = True
+
+# Simulate and display video.
+frames = []
+physics.reset()  # Reset state and time
+while physics.data.time < duration:
+  physics.step()
+  if len(frames) < physics.data.time * framerate:
+    pixels = physics.render(scene_option=scene_option)
+    frames.append(pixels)
+display_video(frames, framerate)
