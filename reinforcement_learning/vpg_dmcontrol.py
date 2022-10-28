464fa38553c64a060c8b30277d3fd5773c02666e
NoNameAA <jee_won_park@alumni.brown.edu>
2022-10-28 01:10:31 -0700

    set up for hop

--- reinforcement_learning/vpg_dmcontrol.py
+++ reinforcement_learning/vpg_dmcontrol.py
@@ -1,3 +1,10 @@
+# Code based on dm_control's tutorial.ipynb
+# Pair programming: Eric Rosen, Jee Won Kyra Park
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 #@title Run to install MuJoCo and `dm_control`
 import distutils.util
 import subprocess
@@ -6,8 +13,6 @@ if subprocess.run('nvidia-smi').returncode:
       'Cannot communicate with GPU. '
       'Make sure you are using a GPU Colab runtime. '
       'Go to the Runtime menu and select Choose runtime type.')
-
-
 # Configure dm_control to use the EGL rendering backend (requires GPU)
 #env MUJOCO_GL=egl
 #make sure to disable the headless option for matplotlib backend to run locally.
@@ -75,18 +80,6 @@ from dm_control.composer import variation
 from dm_control.composer.observation import observable
 from dm_control.composer.variation import distributions
 
-# Font sizes
-SMALL_SIZE = 8
-MEDIUM_SIZE = 10
-BIGGER_SIZE = 12
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
 # Inline video helper function
 if os.environ.get('COLAB_NOTEBOOK_TEST', False):
   # We skip video generation during tests, as it is quite expensive.
@@ -114,40 +107,45 @@ else:
     plt.show()
     return
 
+def reward_togo(episodes):
+    # initialize our list of lists (transitions per episode)
+    episodes_reward_togo = []
+    for episode in episodes:
+        episode_reward_togo = []
+        returns = 0
+        for transition in reversed(episode):
+            returns += transition[2]
+            new_transition = transition
+            # update reward to reward-to-go.
+            new_transition[2] = returns
+            # for the current episode, add the updated transition.
+            episode_reward_togo.append(new_transition)
+        episodes_reward_togo.append(episode_reward_togo)
+    return episodes_reward_togo
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.sm = nn.Softmax(dim=0)
+      # First fully connected layer that takes in state (4D)
+        self.fc1 = nn.Linear(4, 16)
+      # Second fully connected layer that outputs our distribution over 2 actions
+        self.fc2 = nn.Linear(16, 2)
+    def forward(self, input):
+        x = self.fc1(input)
+        x = F.relu(x)
+        x = self.fc2(x)
+        # softmax for the distribution
+        x = self.sm(x)
+        return 
+
+policy = Policy()
+#action_probability = policy.forward(torch.tensor([1,2,3,4], dtype=torch.float))
 # Seed numpy's global RNG so that cell outputs are deterministic. We also try to
 # use RandomState instances that are local to a single cell wherever possible.
 np.random.seed(42)
-'''
-# TODO: define a goal for this rl environment
-#physics = mujoco.Physics.from_xml_string(static_model)
-walker = cmu_humanoid.CMUHumanoid()
-task = go_to_target.GoToTarget(walker, arena=floors.Floor())
-model = control.Environment(walker, task)
-pixels = walker.render()
 
-PIL.Image.fromarray(pixels)
-#plt.imshow(pixels)
 
-#@title Making a video {vertical-output: true}
-
-duration = 2    # (seconds)
-framerate = 30  # (Hz)
-# Visualize the joint axis
-scene_option = mujoco.wrapper.core.MjvOption()
-scene_option.flags[enums.mjtVisFlag.mjVIS_JOINT] = True
-
-# Simulate and display video.
-frames = []
-physics.reset()  # Reset state and time
-while physics.data.time < duration:
-  physics.step()
-  print(physics.step_spec())
-
-  if len(frames) < physics.data.time * framerate:
-    pixels = physics.render(scene_option=scene_option)
-    frames.append(pixels)
-display_video(frames, framerate)
-'''
 # use the benchmark control suite
 max_len = max(len(d) for d, _ in suite.BENCHMARKING)
 for domain, task in suite.BENCHMARKING:
@@ -160,7 +158,7 @@ random_state = np.random.RandomState(42)
 env = suite.load('hopper', 'stand', task_kwargs={'random': random_state})
 
 # Simulate episode with random actions
-duration = 4  # Seconds
+duration = 6  # Seconds
 frames = []
 ticks = []
 rewards = []
@@ -169,17 +167,82 @@ observations = []
 spec = env.action_spec()
 time_step = env.reset()
 
-while env.physics.data.time < duration:
+#TODO: NN that learns a value function (regression)   input: state, output: single number
+# action - softmax learns a normal distribution (normalizes)
+# value function - NN learns.
+# Pytorch DQN
+K=1
+T = 100
+for k in range(K):
+    #list of transitions sampled for the policy
+    #contains [observation, action, reward, observation']
+    episodes = []
+ 
+    #reset environment
+    #observation, info = env.reset()
+    time_step = env.reset()
+    
+    returns = 0
+    episode = []
+    for t in range(T):
+        # compute action probability with the neural net.
+       
+        observation = np.array(time_step.observation.items())
+        print(observation)
+        action_probability = policy.forward(torch.tensor([observation]))
+        #take a random action. make the tensor into np array.
+        action_probability = action_probability.cpu().detach().numpy()[0]
+        print(action_probability)
+        # sample an action according to the current probability distribution.
+        action = random.choices(population=[0,1], weights=action_probability, k=1)[0]
+        #next_observation, reward, terminated, truncated, info = env.step(action)
+        env.step(action)
+        returns += time_step.reward
+        #build transition
+        transition = [observation, time_step.action, time_step.reward, time_step.observation, time_step.terminated]
+        #add transition to list of transitions
+        episode.append(transition)
 
-  action = random_state.uniform(spec.minimum, spec.maximum, spec.shape)
-  time_step = env.step(action)
+        #if we completed an episode, reset the environment and observation
+        if time_step.terminated:
+          #  print("episode has terminated with retursn {R}".format(R=returns))
+            time_step = env.reset()
+            returns = 0
+            episodes.append(episode)
+            episode = []
+        else:
+            #set current observation to new observation
+            observation = time_step.observation
+    #print(episodes[0])
+    episodes_reward_togo = reward_togo(episodes)
+    
 
-  camera0 = env.physics.render(camera_id=0, height=200, width=200)
-  camera1 = env.physics.render(camera_id=1, height=200, width=200)
-  frames.append(np.hstack((camera0, camera1)))
-  rewards.append(time_step.reward)
-  observations.append(copy.deepcopy(time_step.observation))
-  ticks.append(env.physics.data.time)
-
+'''
 html_video = display_video(frames, framerate=1./env.control_timestep())
 
+# Show video and plot reward and observations
+num_sensors = len(time_step.observation)
+
+_, ax = plt.subplots(1 + num_sensors, 1, sharex=True, figsize=(4, 8))
+ax[0].plot(ticks, rewards)
+ax[0].set_ylabel('reward')
+ax[-1].set_xlabel('time')
+
+for i, key in enumerate(time_step.observation):
+  data = np.asarray([observations[j][key] for j in range(len(observations))])
+  ax[i+1].plot(ticks, data, label=key)
+  ax[i+1].set_ylabel(key)
+
+html_video
+
+
+
+
+# define a goal for this rl environment
+#physics = mujoco.Physics.from_xml_string(static_model)
+walker = cmu_humanoid.CMUHumanoid()
+task = go_to_target.GoToTarget(walker, arena=floors.Floor())
+model = control.Environment(walker, task)
+pixels = walker.render()
+PIL.Image.fromarray(pixels)
+'''
